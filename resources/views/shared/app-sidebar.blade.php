5c6d92f6f15f1dc8626fdb7cb381f256f4eb7fc7
Setyo <nsetyo.mail@gmail.com>
2018-11-21 19:57:27 +0700

    Move helpers to separate repository

--- resources/views/shared/app-sidebar.blade.php
+++ resources/views/shared/app-sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
     <nav class="sidebar-nav">
         <ul class="nav">
-            @each('shared.nav-item', App\Menu::all(), 'menu')
+            @each('shared.nav-item', Sty\Helpers\Menu::yaml('menu.yaml'), 'menu')
         </ul>
     </nav>
     <button class="sidebar-minimizer brand-minimizer" type="button"></button>
