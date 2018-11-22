56d00a3c11997ea50e6caea93cbf2d432eae5613
Setyo <nsetyo.mail@gmail.com>
2018-11-22 21:11:55 +0700

    Hapus yang ga perlu

--- app/Helpers.php
+++ app/Helpers.php
@@ -8,16 +8,3 @@ if (!function_exists('crud_response')) {
         return with(new CrudResponse($data, $state))->makeResponse();
     }
 }
-
-if (!function_exists('minutes_to_jam')) {
-    function minutes_to_jam(int $minutes)
-    {
-        $jam = $minutes / 60;
-        $rem = $minutes % 60;
-
-        return sprintf('%s.%s',
-            str_pad($jam, 2, '0', STR_PAD_LEFT),
-            str_pad($rem, 2, '0', STR_PAD_LEFT)
-        );
-    }
-}
