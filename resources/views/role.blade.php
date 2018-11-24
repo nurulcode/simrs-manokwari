9bfdcf79324374f49115259c3176d8a281865b98
Setyo <nsetyo.mail@gmail.com>
2018-11-24 10:38:50 +0700

    Hapus sisa copas

--- resources/views/role.blade.php
+++ resources/views/role.blade.php
@@ -4,25 +4,6 @@
 
 @section('card')
     <data-table v-bind.sync="role" ref="table">
-        <template slot='active' slot-scope="{item}">
-            <button
-                title="Active"
-                type="button"
-                class="btn btn-primary"
-                v-if="item.active"
-                v-on:click.prevent="toggle(item)"
-                >
-                <i class="fa fa-check"></i>
-            </button>
-            <button
-                class="btn btn-danger"
-                title="Inactive"
-                v-else
-                v-on:click="toggle(item)"
-                >
-                <i class="fa fa-times"></i>
-            </button>
-        </template>
         <div slot="form">
             <b-form-group label="Name:" v-bind="role.form.feedback('name')">
                 <input
@@ -74,16 +55,6 @@ window.pagemix.push({
             }
         }
     },
-    methods: {
-        toggle(item) {
-            axios.put(`${item.path}/toggle`)
-                .then(response => {
-                    this.$refs.table.refresh();
-                }).catch(error => {
-                    this.$refs.table.refresh();
-                });
-        }
-    }
 });
 </script>
 @endpush
