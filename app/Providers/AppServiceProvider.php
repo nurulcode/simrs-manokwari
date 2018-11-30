5d90e2f4f6dc01a7aa7e79f7004ddefa4dc05fc1
Setyo <nsetyo.mail@gmail.com>
2018-12-01 04:47:12 +0700

    Install laravel telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 }
