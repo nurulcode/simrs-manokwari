10813c0470fb7504a1da9c90b6b645f7cbfae482
Setyo <nsetyo.mail@gmail.com>
2018-12-01 06:14:23 +0700

    More on permission and role

--- app/PermissionRegistrar.php
+++ app/PermissionRegistrar.php
@@ -5,6 +5,7 @@ namespace App;
 use PDOException;
 use App\Models\Role;
 use App\Models\User;
+use Sty\ResourceModel;
 use App\Models\Permission;
 use Illuminate\Support\Facades\Gate;
 
@@ -29,10 +30,47 @@ class PermissionRegistrar
                 return;
             }
 
+            if (isset($args[0]) && !is_null($policy = Gate::getPolicyFor($args[0]))) {
+                return;
+            }
+
             $permission = Permission::where('name', $ability)->first();
 
-            return $user->hasPermission($permission);
+            if ($permission) {
+                return $user->hasPermission($permission);
+            }
         });
+
+        $operations = [
+            'index'  => 'view_%s_index||view_%s_page',
+            'view'   => 'view_%s_page',
+            'create' => 'create_%s',
+            'update' => 'update_%s',
+            'delete' => 'delete_%s',
+        ];
+
+        foreach ($operations as $key => $permission) {
+            $permissions = explode('||', $permission);
+
+            Gate::define($key, function ($user, $resource) use ($permissions) {
+                if (is_string($resource) && class_exists($resource)) {
+                    $resource = new $resource;
+                }
+
+                if (!$resource instanceof ResourceModel) {
+                    return;
+                }
+                $permkey = $resource->permissionKeyName();
+
+                $result  = $user->can('manage_' . $permkey);
+
+                foreach ($permissions as $permission) {
+                    $result = $result || $user->can(sprintf($permission, $permkey));
+                }
+
+                return $result;
+            });
+        }
     }
 
     public function doAnythingPermission()
