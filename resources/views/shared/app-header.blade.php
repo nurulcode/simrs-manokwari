5aed6b89493d474548d3cf574c430a0ffffa616d
Setyo <nsetyo.mail@gmail.com>
2018-12-04 05:33:24 +0700

    Refactor navbar to be more reusable

--- resources/views/shared/app-header.blade.php
+++ resources/views/shared/app-header.blade.php
@@ -2,10 +2,9 @@
     <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="/">
-        <img src="{{ asset('/images/logo.svg') }}" height="24" style="margin-top: -4px">
-        <h3 class="navbar-brand-full ml-2 mb-1">STYX</h3>
-    </a>
+
+    @include('shared.navbar-brand')
+
     <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
         <span class="navbar-toggler-icon"></span>
     </button>
