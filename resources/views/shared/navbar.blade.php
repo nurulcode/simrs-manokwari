ac587e15cabcdeb96f5873cf209eae4e11e3e4a1
Setyo <nsetyo.mail@gmail.com>
2018-12-06 13:51:19 +0700

    Remove unused

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,9 +1,3 @@
 <ul class="nav navbar-nav ml-auto">
-    <li class="nav-item d-md-down-none">
-        <a class="nav-link" href="#">
-            <i class="icon-list"></i>
-            <span class="badge badge-pill badge-danger">5</span>
-        </a>
-    </li>
     @includeWhen(Auth::check(), 'shared.userdropdown')
 </ul>
