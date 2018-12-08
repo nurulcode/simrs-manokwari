c02d673c323c173012d591da9c25a67467bc3388
Setyo <nsetyo.mail@gmail.com>
2018-12-08 18:06:04 +0700

    Require laravel enum

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,16 @@ class AppServiceProvider extends ServiceProvider
         Response::macro('crud', function ($data, $state = null) {
             return with(new CrudResponse($data, $state))->response();
         });
+
+        Enum::macro('toSelectOptions', function () {
+            return Collection::wrap(self::toSelectArray())->map(function ($item, $key) {
+                return ['value' => $key, 'label' => $item];
+            })->values();
+        });
+
+        Enum::macro('toSelectValue', function ($value) {
+            return ['value' => $value, 'label' => self::getDescription($value)];
+        });
     }
 
     /**
