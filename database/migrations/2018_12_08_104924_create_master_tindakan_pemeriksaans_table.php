edd454adaa2c8a3a2607eb3274035147ad953d05
Setyo <nsetyo.mail@gmail.com>
2018-12-09 16:34:31 +0700

    Rename with master prefix

--- database/migrations/2018_12_08_104924_create_master_tindakan_pemeriksaans_table.php
+++ database/migrations/2018_12_08_104924_create_master_tindakan_pemeriksaans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTindakanPemeriksaansTable extends Migration
+class CreateMasterTindakanPemeriksaansTable extends Migration
 {
     /**
      * Run the migrations.
