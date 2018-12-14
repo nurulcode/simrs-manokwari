e5d37b17dd6ad2ff8adbec08edb505bdb06c4d34
Setyo <nsetyo.mail@gmail.com>
2018-12-14 15:44:33 +0700

    Add date-fns

---
+++ resources/js/__global.js
@@ -0,0 +1,5 @@
+window.addHours     = require('date-fns/add_hours');
+window.subDays      = require('date-fns/sub_days');
+window.startOfToday = require('date-fns/start_of_today');
+window.parse        = require('date-fns/parse');
+window.format       = require('date-fns/format');
