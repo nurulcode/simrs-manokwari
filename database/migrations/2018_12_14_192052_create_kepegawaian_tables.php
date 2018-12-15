c639f364f004facf0c8a6d6cd43905ceb0ed63cb
Setyo <nsetyo.mail@gmail.com>
2018-12-15 10:30:35 +0700

    Add relation

--- database/migrations/2018_12_14_192052_create_kepegawaian_tables.php
+++ database/migrations/2018_12_14_192052_create_kepegawaian_tables.php
@@ -30,11 +30,41 @@ class CreateKepegawaianTables extends Migration
         Schema::create('kualifikasis', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('kategori_id');
-            $table->string('kode')->unique();
+            $table->string('kode');
             $table->string('uraian');
             $table->unsignedInteger('laki_laki')->default(0);
             $table->unsignedInteger('perempuan')->default(0);
             $table->timestamps();
+
+            $table->foreign('kategori_id')
+                ->references('id')
+                ->on('kategori_kualifikasis')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->char('jenis_kelamin', 1);
+            $table->string('tempat_lahir')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('telepon')->nullable();
+            $table->unsignedInteger('jabatan_id')->nullable();
+            $table->unsignedInteger('kualifikasi_id');
+            $table->timestamps();
+
+            $table->foreign('jabatan_id')
+                ->references('id')
+                ->on('jabatans')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+            $table->foreign('kualifikasi_id')
+                ->references('id')
+                ->on('kualifikasis')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
         });
     }
 
@@ -45,6 +75,8 @@ class CreateKepegawaianTables extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('pegawais');
+
         Schema::dropIfExists('kualifikasis');
 
         Schema::dropIfExists('kategori_kualifikasis');
