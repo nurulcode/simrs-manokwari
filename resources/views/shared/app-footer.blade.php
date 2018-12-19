a62cd11c4ec8e61ff4b4a04e6686a74b39717f37
Setyo <nsetyo.mail@gmail.com>
2018-12-19 21:30:11 +0700

    Fix salah company

--- resources/views/shared/app-footer.blade.php
+++ resources/views/shared/app-footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="app-footer">
-    <div> HRDSPL <span class="text-muted">&copy; 2018.</span> </div>
+    <div> SIMRS <span class="text-muted">&copy; 2018.</span> </div>
     <div class="ml-auto">
         <span>Powered by</span>
         <a href="https://coreui.io">CoreUI</a>
