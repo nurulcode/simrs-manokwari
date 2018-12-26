f95380bec9e311088b871fe161a29b5ec1918373
Setyo <nsetyo.mail@gmail.com>
2018-12-26 15:46:00 +0700

    Fix on poliklinik missing

--- app/TindakanPoliklinik.php
+++ app/TindakanPoliklinik.php
@@ -10,7 +10,7 @@ class TindakanPoliklinik
     {
         $tindakan = TindakanPemeriksaan::create(array_except($data, 'polikliniks'));
 
-        $tindakan->polikliniks()->sync(array_pluck($data['polikliniks'], 'id'));
+        $tindakan->polikliniks()->sync(array_pluck(array_get($data, 'polikliniks', []), 'id'));
 
         return $tindakan;
     }
@@ -19,7 +19,7 @@ class TindakanPoliklinik
     {
         $tindakan->update(array_except($data, 'polikliniks'));
 
-        $tindakan->polikliniks()->sync(array_pluck($data['polikliniks'], 'id'));
+        $tindakan->polikliniks()->sync(array_pluck(array_get($data, 'polikliniks', []), 'id'));
 
         return $tindakan;
     }
