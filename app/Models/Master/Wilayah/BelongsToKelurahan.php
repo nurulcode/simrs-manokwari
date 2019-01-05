10947aff99f64ba0e5e6bee8021500f6fd3d49f6
Setyo <nsetyo.mail@gmail.com>
2019-01-06 05:52:07 +0700

    Refactor belongsto kelurahan

--- app/Models/Master/Wilayah/BelongsToKelurahan.php
+++ app/Models/Master/Wilayah/BelongsToKelurahan.php
@@ -2,11 +2,52 @@
 
 namespace App\Models\Master\Wilayah;
 
+use Illuminate\Database\Eloquent\Builder;
+
 /**
  * Belongs to Kelurahan
  */
 trait BelongsToKelurahan
 {
+    use BelongsToProvinsi, BelongsToKotaKabupaten, BelongsToKecamatan;
+
+    public static function bootBelongsToKelurahan()
+    {
+        $table = with(new static)->getTable();
+
+        static::addGlobalScope('kecamatan', function (Builder $builder) use ($table) {
+            $builder->addSubSelect('kecamatan_id',
+                Kelurahan::withoutGlobalScopes(['provinsi', 'kota_kabupaten'])
+                    ->select('kecamatan_id')
+                    ->whereColumn('id', $table . '.kelurahan_id'));
+        });
+
+        static::addGlobalScope('kota_kabupaten', function (Builder $builder) use ($table) {
+            $kecamatan = Kecamatan::withoutGlobalScope('provinsi')
+                ->select('kota_kabupaten_id')
+                ->whereColumn('id', 'kelurahans.kecamatan_id');
+            $kelurahan = Kelurahan::getQuery()
+                ->whereColumn('id', $table . '.kelurahan_id')
+                ->selectSub($kecamatan, 'kota_kabupaten_id');
+
+            $builder->addSubSelect('kota_kabupaten_id', $kelurahan);
+        });
+
+        static::addGlobalScope('provinsi', function (Builder $builder) use ($table) {
+            $kotakab = KotaKabupaten::getQuery()
+                ->select('provinsi_id')
+                ->whereColumn('id', 'kecamatans.kota_kabupaten_id');
+            $kecamatan = Kecamatan::withoutGlobalScope('provinsi')
+                ->whereColumn('id', 'kelurahans.kecamatan_id')
+                ->selectSub($kotakab, 'provinsi_id');
+            $kelurahan = Kelurahan::getQuery()
+                ->whereColumn('id', $table . '.kelurahan_id')
+                ->selectSub($kecamatan, 'provinsi_id');
+
+            $builder->addSubSelect('provinsi_id', $kelurahan);
+        });
+    }
+
     public function kelurahan()
     {
         return $this->belongsTo(Kelurahan::class);
--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -2,23 +2,12 @@
 
 namespace App\Models;
 
-use App\Models\Master\Wilayah\Kelurahan;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Master\Wilayah\BelongsToKelurahan;
-use App\Models\Master\Wilayah\BelongsToKecamatan;
-use App\Models\Master\Wilayah\Kecamatan;
-use App\Models\Master\Wilayah\BelongsToKotaKabupaten;
-use App\Models\Master\Wilayah\KotaKabupaten;
-use App\Models\Master\Wilayah\BelongsToProvinsi;
 
 class Pasien extends Model
 {
-    use SoftDeletes,
-        BelongsToProvinsi,
-        BelongsToKotaKabupaten,
-        BelongsToKecamatan,
-        BelongsToKelurahan;
+    use SoftDeletes, BelongsToKelurahan;
 
     /**
      * The attributes that should be mutated to dates.
@@ -37,38 +26,6 @@ class Pasien extends Model
             }
 
             $model->save();
-        });
-
-        static::addGlobalScope('kecamatan', function (Builder $builder) {
-            $builder->addSubSelect('kecamatan_id',
-                Kelurahan::withoutGlobalScopes(['provinsi', 'kota_kabupaten'])
-                    ->select('kecamatan_id')
-                    ->whereColumn('id', 'pasiens.kelurahan_id'));
-        });
-
-        static::addGlobalScope('kota_kabupaten', function (Builder $builder) {
-            $kecamatan = Kecamatan::withoutGlobalScope('provinsi')
-                ->select('kota_kabupaten_id')
-                ->whereColumn('id', 'kelurahans.kecamatan_id');
-            $kelurahan = Kelurahan::getQuery()
-                ->whereColumn('id', 'pasiens.kelurahan_id')
-                ->selectSub($kecamatan, 'kota_kabupaten_id');
-
-            $builder->addSubSelect('kota_kabupaten_id', $kelurahan);
-        });
-
-        static::addGlobalScope('provinsi', function (Builder $builder) {
-            $kotakab = KotaKabupaten::getQuery()
-                ->select('provinsi_id')
-                ->whereColumn('id', 'kecamatans.kota_kabupaten_id');
-            $kecamatan = Kecamatan::withoutGlobalScope('provinsi')
-                ->whereColumn('id', 'kelurahans.kecamatan_id')
-                ->selectSub($kotakab, 'provinsi_id');
-            $kelurahan = Kelurahan::getQuery()
-                ->whereColumn('id', 'pasiens.kelurahan_id')
-                ->selectSub($kecamatan, 'provinsi_id');
-
-            $builder->addSubSelect('provinsi_id', $kelurahan);
         });
     }
 
