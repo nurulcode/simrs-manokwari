d95b03fa1d6fdec6bdccf722843bd1182977d3e4
Setyo <nsetyo.mail@gmail.com>
2019-01-06 09:22:56 +0700

    Fix datatable user

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable implements Sty\ResourceModel
         Sty\HasPath,
         Sty\HasPolicy,
         Sty\Searchable,
+        Sty\MethodOrderable,
         HasApiTokens,
         HasRoles,
         Notifiable,
