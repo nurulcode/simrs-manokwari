832ab00aa00b267ba170f8e83da5c343dad45f2c
Setyo <nsetyo.mail@gmail.com>
2019-01-18 11:10:04 +0700

    Fix form missing kategori

--- resources/views/master/jenis-registrasi.blade.php
+++ resources/views/master/jenis-registrasi.blade.php
@@ -16,6 +16,17 @@
                     >
                 </input>
             </b-form-group>
+            <b-form-group v-bind="resource.form.feedback('kategori')">
+                <b slot="label">Kategori Registrasi:</b>
+                <b-form-select
+                    :options="{{ json_encode(App\Enums\KategoriRegistrasi::toSelectOptions()) }}"
+                    v-on:change="resource.errors.clear('kategori')"
+                    v-model="resource.form.kategori">
+                    <template slot="first">
+                        <option :value="null" disabled>Pilih Kategori Registrasi</option>
+                    </template>
+                </b-form-select>
+            </b-form-group>
         </div>
     </data-table>
 @endsection
@@ -32,7 +43,10 @@ window.pagemix.push({
                     key      : 'uraian',
                     sortable : true,
                 }],
-                form: new Form({uraian: null}),
+                form: new Form({
+                    uraian  : null,
+                    kategori: null
+                }),
             }
         }
     },
