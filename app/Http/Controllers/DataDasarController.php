3f76be3411402e9dee03781f6fbddae6753467c0
Setyo <nsetyo.mail@gmail.com>
2019-07-25 05:00:58 +0700

    Data dasar #1

---
+++ app/Http/Controllers/DataDasarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataDasar;
+use Illuminate\Http\Request;
+
+class DataDasarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DataDasar  $dataDasar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DataDasar $dataDasar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DataDasar  $dataDasar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DataDasar $dataDasar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DataDasar  $dataDasar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DataDasar $dataDasar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DataDasar  $dataDasar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DataDasar $dataDasar)
+    {
+        //
+    }
+}
---
+++ database/factories/DataDasarFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\DataDasar::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
